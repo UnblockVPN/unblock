1b76a11d1ed3d96bd3caf2cdc583d00672567b59
0xCosydell <david.awatere@gmail.com>
2023-05-29 22:02:28 +0100

    Update supabase-admin.ts

--- utils/supabase-admin.ts
+++ utils/supabase-admin.ts
@@ -6,6 +6,7 @@ import { toDateTime } from './helpers';
 
 import { Customer, UserDetails, Price, Product } from 'types';
 import type { Database } from 'types_db';
+const status = "active";
 
 // Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
 // as it has admin priviliges and overwrites RLS policies!
