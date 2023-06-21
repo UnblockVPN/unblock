6c965adb7b4fb4906973ad1043581cf602d18008
0xCosydell <david.awatere@gmail.com>
2023-06-21 12:56:12 +0200

    rebase

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,21 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/davidawatere/GitHub/_saas/unblock/unblock/app/layout.tsx": [
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/styles/main.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/styles/chrome-bug.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/Navbar/Navbar.module.css"
+    ],
+    "/Users/davidawatere/GitHub/_saas/unblock/unblock/app/page.tsx": [
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/LoadingDots/LoadingDots.module.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/Button/Button.module.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/davidawatere/GitHub/_saas/unblock/unblock/app/page": [],
+    "/Users/davidawatere/GitHub/_saas/unblock/unblock/app/signin/page": [
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/Navbar/Navbar.module.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/styles/main.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/styles/chrome-bug.css"
+    ]
+  }
+}
