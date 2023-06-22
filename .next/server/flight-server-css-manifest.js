1b4c0bdd98789f2fe059fc985d42d11559e1d851
0xCosydell <david.awatere@gmail.com>
2023-06-22 12:47:29 +0200

    .

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,4 +1,20 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {},
-  "cssModules": {}
+  "cssImports": {
+    "/Users/davidawatere/GitHub/_saas/unblock/unblock/app/layout.tsx": [
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/styles/main.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/Navbar/Navbar.module.css"
+    ],
+    "/Users/davidawatere/GitHub/_saas/unblock/unblock/app/support/page.tsx": [
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/Button/Button.module.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/LoadingDots/LoadingDots.module.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/davidawatere/GitHub/_saas/unblock/unblock/app/support/page": [
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/Navbar/Navbar.module.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/styles/main.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/LoadingDots/LoadingDots.module.css",
+      "/Users/davidawatere/GitHub/_saas/unblock/unblock/components/ui/Button/Button.module.css"
+    ]
+  }
 }
