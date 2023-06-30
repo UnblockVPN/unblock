b1d4f0e02f22eb54d644fce797a54596ab899649
0xCosydell <david.awatere@gmail.com>
2023-06-30 19:04:08 +0200

    download

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -1,4 +1,5 @@
 import ManageSubscriptionButton from './ManageSubscriptionButton';
+import Download from '@/components/ui/Download';
 import {
   getSession,
   getUserDetails,
@@ -12,12 +13,14 @@ import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
+import { getActiveProductsWithPrices } from '@/app/supabase-server';
 
 export default async function About() {
-  const [session, userDetails, subscription] = await Promise.all([
+  const [session, userDetails, subscription, products] = await Promise.all([
     getSession(),
     getUserDetails(),
-    getSubscription()
+    getSubscription(),
+    getActiveProductsWithPrices()
   ]);
 
   const user = session?.user;
@@ -61,25 +64,6 @@ export default async function About() {
   };
 
   return (
-    <section className="mb-32 bg-black">
-      <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
-        <div className="sm:align-center sm:flex sm:flex-col">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            DOWNLOAD
-          </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            We partnered with Stripe for a simplified billing.
-          </p>
-        </div>
-      </div>
-      
-    </section>
-  );
-}
-
-interface Props {
-  title: string;
-  description?: string;
-  footer?: ReactNode;
-  children: ReactNode;
+    <Download session={session} user={user} subscription={subscription} products={products}/>
+    );
 }
--- components/ui/Download/Download.tsx
+++ components/ui/Download/Download.tsx
@@ -2,11 +2,8 @@ import { Session, User } from '@supabase/supabase-js';
 import Pricing from '@/components/Pricing';
 import Link from 'next/link';
 import Image from "next/image";
-import Container from '@/components/ui/Hero/Container';
-import heroImg from "../../../public/couple_02.png";
+import Container from '@/components/ui/Download/Container';
 import s from '../Navbar/Navbar.module.css';
-
-
 
 interface Props {
   session: Session | null;
@@ -16,37 +13,33 @@ interface Props {
 }
 
 
-const Hero: React.FC<Props> = ({ session, user, subscription, products }) => {
+const Download: React.FC<Props> = ({ session, user, subscription, products }) => {
   return (
     <>
-          <div className={`${s.heroContainer} mx-auto max-w-[1920px]  py-4 `}>
+          <div className={`${s.downloadContainer} mx-auto max-w-[1920px]  py-4 `}>
 
-      <Container className="mx-auto max-w-[1920px]  py-4 ">
-        <div className="flex flex-col mt-2 max-w-6xl items-center px-4 text-left w-full">
-          <div className="max-w-2xl">
-            <h2 className="text-left text-2xl mb-4  font-extrabold sm:text-2xl">
-              Protect your digital life 24/7
-            </h2>
-            <h1 className="text-left text-4xl mb-10 font-extrabold sm:text-4xl whitespace-nowrap">
-              Get Premium VPN free for 1 month
-            </h1>
-            <p className="text-left mb-10 leading-normal text-xl sm:text-2xl">
-              Only $9.99/month after. Cancel anytime.
-            </p>
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <Link href="/order" className="px-6 py-3 border border-black border-2 font-medium text-center bg-black rounded-full sm:text-white transition duration-500 ease-in-out transform hover:scale-110">
-                GET STARTED
-              </Link>
-              <Link href="#offer" scroll={false} className="bg-blue-600 border border-white border-2 px-6 py-3 font-medium text-center rounded-full text-white transition duration-500 ease-in-out transform hover:scale-110">
-                VIEW PLANS
-              </Link>
-            </div>
-            <div className="flex flex-col mt-4 items-start text-sm font-medium space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              Terms and conditions apply. 1 month free not available for users who have already tried Premium.
-            </div>
-          </div>
-        </div>
-      </Container>
+          <Container className="mx-auto max-w-[1920px] flex items-center justify-center py-4 ">
+  <div className="flex flex-col mt-2 max-w-6xl justify-center items-center px-4 text-center w-full">
+    <div className="max-w-2xl">
+    <div className="flex justify-center items-center">
+        <img src="/download.png" alt="Download" className="h-24 w-32 object-contain"/>
+      </div>
+      <h1 className=" text-5xl mt-6 mb-10 text-black font-extrabold sm:text-5xl whitespace-nowrap">
+        Download UnblockVPN
+      </h1>
+      <p className=" mb-10 leading-normal text-black text-l sm:text-l">
+      Connect to 158 countries from your device.
+      </p>
+      <div className="flex justify-center  flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+        <Link href="/order" className="px-6 py-3 font-medium text-center bg-gray-200 rounded-full text-black sm:text-black transition duration-500 ease-in-out transform hover:scale-110">
+          Download
+        </Link>
+
+      </div>
+    </div>
+  </div>
+</Container>
+
       </div>
 
      <Container className="mx-auto max-w-[1920px] flex bg-white text-black flex-col items-center  sm:py-12 sm:px-6">
@@ -108,4 +101,4 @@ const Hero: React.FC<Props> = ({ session, user, subscription, products }) => {
   );
 }
 
-export default Hero;
+export default Download;
