6aa72e58ced2a2cd104e013c131460719658030d
0xCosydell <david.awatere@gmail.com>
2023-06-30 13:53:54 +0200

    Update Pricing.tsx

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -95,6 +95,7 @@ export default function Pricing({
     );
 
   if (products.length === 1)
+  
   return (
       <section className="">
         <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
@@ -162,7 +163,8 @@ export default function Pricing({
       </section>
     );
 
-//ACTIVE OFFER
+    if (products.length === 3)
+//ACTIVE OFFER MONTHLY ANNUAL SWITCH
     return (
     <section className="">
       <div className="max-w-6xl px-4 py-2 mx-auto sm:py-2 sm:px-6 lg:px-8">
@@ -213,7 +215,7 @@ export default function Pricing({
 
 
         {/* ACTIVE OFFER */}
-        <div className="">
+       
         <div className="relative flex flex-wrap mt-12 mx-auto w-3/4 max-w-2xl gap-8 justify-center items-center sm:justify-start sm:grid sm:grid-cols-1">
           {products.map((product) => {
             const price = product?.prices?.find(
@@ -233,23 +235,84 @@ export default function Pricing({
                   {
                     'border border-pink-500': subscription
                       ? product.name === subscription?.prices?.products?.name
-                      : product.name === 'Freelancer'
+                      : product.name === 'Family'
                   }
                 )}
               >
                 <div className="p-6">
+
+                <div className="w-32 h-8 rounded-md mb-2 font-semibold bg-blue-600 text-white flex items-center justify-center text-white">
+                    1 month free
+                  </div>
+                  
                   <h2 className="text-2xl font-semibold leading-6 text-white">
                     {product.name}
                   </h2>
-                  <p className="mt-4 text-white text-zinc-300">{product.description}</p>
-                  <p className="mt-8">
-                    <span className="text-white text-5xl font-extrabold white">
-                      {priceString}
+
+                  <p className="mt-2">
+                    <span className="text-white white">
+                      {priceString} 
                     </span>
-                    <span className="text-white text-base font-medium text-zinc-100">
-                      /{billingInterval}
+                    <span className="text-white text-base font-medium">
+                      /{billingInterval} after offer period
                     </span>
                   </p>
+                  <p className="mt-4 text-white text-zinc-300">{product.description}</p>
+                 
+
+                  <div class="mt-4 w-9/10 h-px bg-gray-300"></div>
+
+
+                  <ul class="flex flex-col gap-4 mt-6">
+                    <li class="flex items-center space-x-2">
+                      <div class="w-6 h-6">
+                        <svg
+                          role="img"
+                          focusable="false"
+                          height="24"
+                          width="24"
+                          viewBox="0 0 24 24"
+                          aria-label=""
+                          class="text-white stroke-current"
+                        >
+                          <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#ffffff"></polyline>
+                        </svg>
+                      </div>
+                      <p class="text-white">Ad-free music listening</p>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                      <div class="w-6 h-6">
+                        <svg
+                          role="img"
+                          focusable="false"
+                          height="24"
+                          width="24"
+                          viewBox="0 0 24 24"
+                          aria-label=""
+                          class="text-white stroke-current"
+                        >
+                          <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#ffffff"></polyline>
+                        </svg>
+                      </div>
+                      <p class="text-white">Play anywhere - even offline</p>
+                    </li>
+                    <li class="flex items-center space-x-2">
+                      <div class="w-6 h-6">
+                        <svg
+                          role="img"
+                          focusable="false"
+                          height="24"
+                          width="24"
+                          viewBox="0 0 24 24"
+                          aria-label=""
+                          class="text-white stroke-current"
+                        >
+                          <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#ffffff"></polyline>
+                        </svg>
+                      </div>
+                      <p class="text-white">On-demand playback</p>
+                    </li>
+                  </ul>
                   <Button
                     variant="slim"
                     type="button"
@@ -262,18 +325,243 @@ export default function Pricing({
                       ? 'Manage'
                       : 'Subscribe'}
                   </Button>
+
+
+                  <p className="mt-2">
+                    <span className="text-white text-base text-xs font-medium">
+                    Terms and conditions apply. 1 month free not available for users who have already tried Premium.
+                    </span>
+                  </p>                 
                 </div>
-              </div>
-              
+              </div>     
             );
           })}
         </div>
-        </div>
+        
+{/* ACTIVE OFFER END */}
+
         <LogoCloud />
+
+      </div>
+    </section>
+    )
+
+
+
+  if (products.length === 2) {
+    const targetProduct1 = products.find(product => product.name === 'Duo');
+    const targetProduct2 = products.find(product => product.name === 'Family');
+  
+    return (
+      <section className="">
+        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:flex-col sm:align-center">
+            <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+              Pricing Plans...
+            </h1>
+            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+              Start building for free, then add a site plan to go live. Account plans unlock additional features.
+            </p>
+            <div className="relative flex self-center mt-12 border rounded-lg bg-zinc-900 border-zinc-800">
+              <div className="border border-pink-500 border-opacity-50 divide-y rounded-lg shadow-sm bg-zinc-900 divide-zinc-600">
+                <div className="p-6 py-2 m-1 text-2xl font-medium text-white rounded-md shadow-sm border-zinc-800 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">
+                  {targetProduct1.name}
+                </div>
+              </div>
+            </div>
+            <div>
+              {targetProduct1.prices?.map((price) => {
+                const priceString = price.unit_amount && new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: price.currency!,
+                  minimumFractionDigits: 0
+                }).format(price.unit_amount / 100);
+  
+                return (
+                  <div
+                    key={price.interval}
+                    className="divide-y rounded-lg shadow-sm divide-zinc-600 bg-zinc-900"
+                  >
+                    <div className="p-6">
+                      <p>
+                        <span className="text-5xl font-extrabold white">
+                          {priceString}
+                        </span>
+                        <span className="text-base font-medium text-zinc-100">
+                          /{price.interval}
+                        </span>
+                      </p>
+                      <p className="mt-4 text-zinc-300">{price.description}</p>
+                      <Button
+                        variant="slim"
+                        type="button"
+                        loading={priceIdLoading === price.id}
+                        onClick={() => handleCheckout(price)}
+                        className="block w-full py-2 mt-12 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900 "
+                      >
+                        {targetProduct1.name === subscription?.prices?.products?.name
+                          ? 'Manage'
+                          : 'Subscribe'}
+                      </Button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  
+
+    return (
+      <section className="">
+        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:flex-col sm:align-center">
+            <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+              Pricing Plans...
+            </h1>
+            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+              Start building for free, then add a site plan to go live. Account plans unlock additional features.
+            </p>
+            <div className="relative flex self-center mt-12 border rounded-lg bg-zinc-900 border-zinc-800">
+              <div className="border border-pink-500 border-opacity-50 divide-y rounded-lg shadow-sm bg-zinc-900 divide-zinc-600">
+                <div className="p-6 py-2 m-1 text-2xl font-medium text-white rounded-md shadow-sm border-zinc-800 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">
+                  {targetProduct2.name}
+                </div>
+              </div>
+            </div>
+            <div>
+              {targetProduct2.prices?.map((price) => {
+                const priceString = price.unit_amount && new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: price.currency!,
+                  minimumFractionDigits: 0
+                }).format(price.unit_amount / 100);
+  
+                return (
+                  <div
+                    key={price.interval}
+                    className="divide-y rounded-lg shadow-sm divide-zinc-600 bg-zinc-900"
+                  >
+                    <div className="p-6">
+                      <p>
+                        <span className="text-5xl font-extrabold white">
+                          {priceString}
+                        </span>
+                        <span className="text-base font-medium text-zinc-100">
+                          /{price.interval}
+                        </span>
+                      </p>
+                      <p className="mt-4 text-zinc-300">{price.description}</p>
+                      <Button
+                        variant="slim"
+                        type="button"
+                        loading={priceIdLoading === price.id}
+                        onClick={() => handleCheckout(price)}
+                        className="block w-full py-2 mt-12 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900 "
+                      >
+                        {targetProduct2.name === subscription?.prices?.products?.name
+                          ? 'Manage'
+                          : 'Subscribe'}
+                      </Button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  
+
+}
+
+
+
+if (products.length === 3) {
+  const targetProduct = products.find(product => product.name === 'Family');
+
+  if (!targetProduct) {
+    return (
+      <section>
+        {/* Render fallback content */}
+      </section>
+    );
+  }
+
+  return (
+    <section className="">
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:flex-col sm:align-center">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+            Pricing Plans...
+          </h1>
+          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+            Start building for free, then add a site plan to go live. Account plans unlock additional features.
+          </p>
+          <div className="relative flex self-center mt-12 border rounded-lg bg-zinc-900 border-zinc-800">
+            <div className="border border-pink-500 border-opacity-50 divide-y rounded-lg shadow-sm bg-zinc-900 divide-zinc-600">
+              <div className="p-6 py-2 m-1 text-2xl font-medium text-white rounded-md shadow-sm border-zinc-800 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">
+                {targetProduct.name}
+              </div>
+            </div>
+          </div>
+          <div>
+            {targetProduct.prices?.map((price) => {
+              const priceString = price.unit_amount && new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: price.currency!,
+                minimumFractionDigits: 0
+              }).format(price.unit_amount / 100);
+
+              return (
+                <div
+                  key={price.interval}
+                  className="divide-y rounded-lg shadow-sm divide-zinc-600 bg-zinc-900"
+                >
+                  <div className="p-6">
+                    <p>
+                      <span className="text-5xl font-extrabold white">
+                        {priceString}
+                      </span>
+                      <span className="text-base font-medium text-zinc-100">
+                        /{price.interval}
+                      </span>
+                    </p>
+                    <p className="mt-4 text-zinc-300">{price.description}</p>
+                    <Button
+                      variant="slim"
+                      type="button"
+                      loading={priceIdLoading === price.id}
+                      onClick={() => handleCheckout(price)}
+                      className="block w-full py-2 mt-12 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900 "
+                    >
+                      {targetProduct.name === subscription?.prices?.products?.name
+                        ? 'Manage'
+                        : 'Subscribe'}
+                    </Button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </section>
   );
+
+
+
 }
+
+}
+
+
+
+
+
 
 function LogoCloud() {
   return (
@@ -328,6 +616,7 @@ function LogoCloud() {
           </a>
         </div>
       </div>
+      
     </div>
   );
 }
