641081a1a8a2511f6b6dea25ddae00a3dbb38473
0xCosydell <david.awatere@gmail.com>
2023-07-02 15:25:00 +0200

    Update route.ts

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -1,5 +1,29 @@
-// pages/api/hello.js
 export default function handler(req, res) {
+    try {
+      const { method } = req;
+  
+      if (method === 'GET') {
+        const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        res.status(200).json({ ip: ipAddress });
+      } else {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).json({ message: `Method ${method} Not Allowed` });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+  
+
+
+
+
+
+
+
+
+// pages/api/hello.js
+/* export default function handler(req, res) {
     let ip;
   
     if (req.headers["x-forwarded-for"]) {
@@ -14,4 +38,4 @@ export default function handler(req, res) {
   
     console.log(ip)
     res.status(200).json({ name: 'John Doe' })
-  }
+  } */
