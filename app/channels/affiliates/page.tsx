0318c174f7c2fb42c11dd8c1e2234ef8dc7f0327
0xCosydell <david.awatere@gmail.com>
2023-12-30 08:38:13 +0100

    Update page.tsx

--- app/channels/affiliates/page.tsx
+++ app/channels/affiliates/page.tsx
@@ -1,32 +1,85 @@
 // app/channels/affiliates/page.tsx
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
 
-export default function ChannelsAffiliates() {
-  const router = useRouter();
+import {
+  getSession,
+  getUserDetails,
+  getSubscription
+} from '@/app/supabase-server';
+import { Database } from '@/types_db';
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { ReactNode } from 'react';
 
-  useEffect(() => {
-    // Client-side logic here
-    async function fetchSessionAndRedirect() {
-      // Example: Fetch session data from an API endpoint
-      const sessionResponse = await fetch('/api/getSession');
-      const session = await sessionResponse.json();
+export default async function Affiliates() {
+  const [session, userDetails, subscription] = await Promise.all([
+    getSession(),
+    getUserDetails(),
+    getSubscription()
+  ]);
 
-      if (!session?.user) {
-        router.push(`/signin?redirect=${encodeURIComponent('/channels/affiliates')}`);
-      } else {
-        // Handle logged-in user scenario
-      }
+  const user = session?.user;
+
+
+  const subscriptionPrice =
+    subscription &&
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: subscription?.prices?.currency!,
+      minimumFractionDigits: 0
+    }).format((subscription?.prices?.unit_amount || 0) / 100);
+
+  const updateName = async (formData: FormData) => {
+    'use server';
+
+    const newName = formData.get('name') as string;
+    const supabase = createServerActionClient<Database>({ cookies });
+    const session = await getSession();
+    const user = session?.user;
+    const { error } = await supabase
+      .from('users')
+      .update({ full_name: newName })
+      .eq('id', user?.id);
+    if (error) {
+      console.log(error);
     }
+    revalidatePath('/channels/affiliates');
+  };
 
-    fetchSessionAndRedirect();
-  }, [router]);
+  const updateEmail = async (formData: FormData) => {
+    'use server';
+
+    const newEmail = formData.get('email') as string;
+    const supabase = createServerActionClient<Database>({ cookies });
+    const { error } = await supabase.auth.updateUser({ email: newEmail });
+    if (error) {
+      console.log(error);
+    }
+    revalidatePath('/channels/affiliates');
+  };
 
   return (
     <section className="mb-32 bg-black">
-      {/* Your component JSX */}
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
+        <div className="sm:align-center sm:flex sm:flex-col">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+            Affiliates
+          </h1>
+          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+            Call to action here
+          </p>
+        </div>
+      </div>
+      
     </section>
   );
 }
 
-ChannelsAffiliates.client = true; // Mark this component as a client component
+interface Props {
+  title: string;
+  description?: string;
+  footer?: ReactNode;
+  children: ReactNode;
+}
