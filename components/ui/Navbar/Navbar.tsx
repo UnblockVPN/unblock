c8bc7f00ba59913c14b15e78118469811578bd75
0xCosydell <david.awatere@gmail.com>
2024-01-08 14:32:38 +0100

    Update Navbar.tsx

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -13,6 +13,7 @@ export default async function Navbar() {
 
   return (
     <nav className={s.root}>
+  
       <a href="#skip" className="sr-only focus:not-sr-only">
         Skip to content
       </a>
